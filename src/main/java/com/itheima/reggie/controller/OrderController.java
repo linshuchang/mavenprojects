b5ae90cbb3c2ee0f115b1608755edaede9037dc1
lin <lin@qq.com>
2022-12-16 10:25:17 +0800

    OrderController master分支修改main方法输出push GitHub

--- src/main/java/com/itheima/reggie/controller/OrderController.java
+++ src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -16,6 +16,7 @@ public class OrderController {
     public static void main(String[] args) {
         System.out.println("3");
         System.out.println("2");
+        System.out.println("push github");
     }
 }
 
